20cdf1b50a2059ea004bbc8167d701a60c62119f
twahir21 <twahirsudy3@gmail.com>
2025-11-10 15:30:37 +0300

    adding some configs

--- src/routes/plugin.tsx
+++ src/routes/plugin.tsx
@@ -2,15 +2,15 @@
 // define routeAction for posting to API
 // this is SSR not executed by browser
 
-import { routeAction$, routeLoader$, z, zod$ } from "@builder.io/qwik-city";
+import { routeAction$, z, zod$ } from "@builder.io/qwik-city";
 import { createApi } from "~/api/sample.api";
 
 // example
-export const useUsers = routeLoader$(async ({ redirect, cookie, pathname }) => {
-    const token = cookie.get("jwtToken")?.value;
-    if (!token && pathname.startsWith("/admin")) throw redirect(302, "/auth");
-    return await createApi.user(token!)
-});
+// export const useUsers = routeLoader$(async ({ redirect, cookie, pathname }) => {
+//     const token = cookie.get("jwtToken")?.value;
+//     if (!token && pathname.startsWith("/admin")) throw redirect(302, "/auth");
+//     return await createApi.user(token!)
+// });
 
 export const useLogin = routeAction$(async(data, { cookie, redirect }) => {
     const result = await createApi.login(data);
